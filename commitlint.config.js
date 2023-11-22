1dc2ae3aa310ba69a66cdf21b838cd34546e7351
Michael S. Liebman <michael.liebman@gmail.com>
2023-11-21 22:37:33 -0500

    chore: :technologist: remove broken issue prefix rule (#1)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  parserPreset: {
-    parserOpts: {
-      issuePrefixes: ['(#']
-    }
-  },
   rules: {
     'references-empty': [2, 'never']
   }
