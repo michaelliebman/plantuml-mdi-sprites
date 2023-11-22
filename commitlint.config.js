0972a9ad96d36e37d9e73c931498715f6bacaacf
Michael S. Liebman <michael.liebman@gmail.com>
2023-11-21 22:33:33 -0500

    chore: :technologist: add commitlint rule for issue in reference (#1)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  parserPreset: {
+    parserOpts: {
+      issuePrefixes: ['(#']
+    }
+  },
+  rules: {
+    'references-empty': [2, 'never']
+  }
 };
